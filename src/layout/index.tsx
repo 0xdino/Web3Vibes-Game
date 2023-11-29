bf69fd5c14094a1daf38948dc0d376bbb484ee32
Dmitry0x0 <volov.dmit@yandex.ru>
2023-11-29 23:46:55 +0300

    meta

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -22,10 +22,26 @@ export default function Layout({ title, description, children }: ILayoutProps) {
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <meta name="theme-color" content="#001320" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link
           rel="preload"
           href={Exo2}
